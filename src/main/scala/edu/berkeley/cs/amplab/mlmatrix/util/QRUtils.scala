2415a8566e4fe063fb284e8446aa033c111782c4
Rebecca Roelofs <roelofs@berkeley.edu>
2015-07-09 15:52:53 -0700

    adding comment to explain that DORMQR is not thread safe

--- src/main/scala/edu/berkeley/cs/amplab/mlmatrix/util/QRUtils.scala
+++ src/main/scala/edu/berkeley/cs/amplab/mlmatrix/util/QRUtils.scala
@@ -138,6 +138,7 @@ object QRUtils {
 
     val lwork1 = if(info.`val` != 0) result.cols else work(0).toInt
     val workspace = new Array[Double](lwork1)
+    // NOTE: Synchronize access to dormqr as it is not always thread safe
     QRUtils.synchronized {
       lapack.dormqr("L", trans, result.rows, result.cols, T.length, Y.data, Y.rows, T,
         result.data, result.rows, workspace, workspace.length, info)
